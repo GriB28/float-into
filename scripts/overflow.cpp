97a2f572fd2530cd89eff9a1fb8dacc214bf7411
|GriB28| <gp.off.dev@gmail.com>
2025-10-26 16:35:28 +0300

    overflow fix

--- scripts/overflow.cpp
+++ scripts/overflow.cpp
@@ -5,24 +5,26 @@
 
 using namespace color;
 using std::cout;
+using std::string;
 
 
-void print(const unsigned &pow, const string &current) {
+void print(const unsigned &pow, const string &current, const float &value) {
     cout << YELLOW << "10^" << pow << RESET << "\t:\t" << YELLOW;
     utils::print_float_binary_line(current);
-    cout << RESET << '\n';
+    cout << RESET << "\t:\t" << YELLOW << value << RESET << '\n';
 }
 
 
 int main() {
     cout << std::fixed;
     cout.precision(2);
+
     cout << YELLOW;
     float number = 1.0;
     unsigned power = 0;
     string current_bin = utils::float2bin(number), previous_bin;
 
-    print(power, current_bin);
+    print(power, current_bin, number);
 
     while (current_bin != previous_bin) {
         number *= 10;
@@ -30,7 +32,7 @@ int main() {
         previous_bin = current_bin;
         current_bin = utils::float2bin(number);
 
-        print(power, current_bin);
+        print(power, current_bin, number);
     }
 
     cout << "infinite cycle from this point...\n";
